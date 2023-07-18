91c68f9dc874fcb992ebda35d82bc6a07bc92fbc
Rob0tWizard <132695066+Rob0tWizard@users.noreply.github.com>
2023-07-18 16:30:42 +0700

    Add files via upload

---
+++ un.py
@@ -0,0 +1,25 @@
+alphabet_ru = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у',
+               'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']
+
+alphabet_en = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z']
+
+step = int(input("Введите шаг: "))
+lang = input("Выберете язык RU/EN: ").lower()
+message = str(input('Введите сообщение для дешифровки: ').lower())
+result = ""
+
+if lang == 'ru' or lang == "ру":
+    for item in message:
+        try:
+            place = alphabet_ru.index(item)
+            if place + step > len(alphabet_ru):
+                new_place = (place - step) + int(len(alphabet_ru))
+            else:
+                new_place = place - step
+        except ValueError:
+            result += item
+
+        result += alphabet_ru[new_place]
+
+print(result)
