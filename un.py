64590ac8c5db9f1704992dd50a063ebbe8b31545
Rob0tWizard <132695066+Rob0tWizard@users.noreply.github.com>
2023-07-22 23:09:24 +0700

    Add files via upload

--- un.py
+++ un.py
@@ -15,11 +15,26 @@ if lang == 'ru' or lang == "ру":
             place = alphabet_ru.index(item)
             if place + step > len(alphabet_ru):
                 new_place = (place - step) + int(len(alphabet_ru))
+                result += alphabet_ru[new_place]
             else:
                 new_place = place - step
+                result += alphabet_ru[new_place]
         except ValueError:
             result += item
 
-        result += alphabet_ru[new_place]
+elif lang == 'en' or lang == "eng":
+    for item in message:
+        try:
+            place = alphabet_en.index(item)
+            if place + step >= len(alphabet_en):
+                new_place = (place - step) % int(len(alphabet_en))
+                result += alphabet_en[new_place]
+            else:
+                new_place = place - step
+                result += alphabet_en[new_place]
+        except ValueError:
+            result += item
+
+
 
 print(result)
